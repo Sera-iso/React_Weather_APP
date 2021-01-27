2866d8416756b078333210289c67b0c1dc40633a
Sera_Is <isonserena@hotmail.it>
2021-01-27 23:02:02 +0100

    Added rel="noreferrer" to footer link

--- src/Footer.js
+++ src/Footer.js
@@ -4,7 +4,7 @@ import "./Footer.css";
 export default function Footer() {
     return (
         <div className="Footer">
-            <p><a href="https://github.com/Sera-iso/React_Weather_APP/tree/master/src" target="_blank">Open source coded</a> by Sera</p>
+            <p><a href="https://github.com/Sera-iso/React_Weather_APP/tree/master/src" target="_blank" rel="noreferrer">Open source coded</a> by Sera</p>
         </div>
     )
 
