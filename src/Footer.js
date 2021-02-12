61c19791525cdc9bac4aa1584cf853f07d391aa3
Sera_Is <isonserena@hotmail.it>
2021-02-12 22:22:33 +0100

    Added spacing in Footer.js

--- src/Footer.js
+++ src/Footer.js
@@ -5,9 +5,7 @@ export default function Footer() {
     return (
         <div className="Footer">
             <footer>
-                <a href="https://github.com/Sera-iso/React_Weather_APP/tree/master/src" target="_blank" rel="noreferrer">Open source coded</a> 
-                by 
-                <a href="https://www.linkedin.com/in/serena-isone-b3994943/?locale=en_US" target="_blank" rel="noreferrer">Sera</a>
+                <a href="https://github.com/Sera-iso/React_Weather_APP/tree/master/src" target="_blank" rel="noreferrer">Open source coded</a> by <a href="https://www.linkedin.com/in/serena-isone-b3994943/?locale=en_US" target="_blank" rel="noreferrer">Sera</a>
             </footer>
         </div>
     )
