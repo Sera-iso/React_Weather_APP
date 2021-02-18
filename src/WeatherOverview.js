75e45309ded974eb3b957f6116e275b518b8ac7c
Sera_Is <isonserena@hotmail.it>
2021-02-18 21:44:30 +0100

    Optimized CSS of newly introduced "feels_like" API call

--- src/WeatherOverview.js
+++ src/WeatherOverview.js
@@ -10,10 +10,10 @@ export default function WeatherOverview( {data} ) {
           <h1>{data.city}, {data.country}</h1>
           <WeatherTemperature celsius={Math.round(data.temp)} />
           <ul>
+            <li className="perception">Feels like {Math.round(data.perception)}°C</li>
             <li className="weather-conditions">
               <WeatherIcon iconCode={data.icon} iconAnimated />
               {data.condition}</li>
-            <li>{data.perception}</li>
             <li className="other-measurements">Humidity: {data.humidity}% | Wind: {Math.round(data.wind)}km/h</li>
           </ul>
           <FormattedDate date={data.date} />
