961cbe611819407f5d18a3de601287b2c0682cbb
Sera_Is <isonserena@hotmail.it>
2021-03-01 23:29:13 +0100

    Removed console.log to test API calls

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -23,7 +23,7 @@ export default function WeatherForecast( {city} ) {
         const apiKey = `c3fdeae7b368bbda2e09bebfb67775c6`;
         const forecastUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${apiKey}&units=metric&cnt=5`;
         axios.get(forecastUrl).then(returnForecastData);
-    console.log(1);
+
         return null;
     }
 }
